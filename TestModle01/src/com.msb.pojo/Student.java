e5c5ad391e0c84971f635b645c2bd153d55c92fc
zhaoss <chinazss@126.com>
2022-05-20 17:21:17 +0800

    添加了数据

--- TestModle01/src/com.msb.pojo/Student.java
+++ TestModle01/src/com.msb.pojo/Student.java
@@ -1,10 +1,15 @@
 package com.msb.pojo;
 
+import java.io.Serializable;
+
 /**
  * @auther: zhaoss
  * @date: 2022\5\20 0020 - 05 - 20 - 17:07
  * @description: com.msb.pojo
  * @version: 1.0
  */
-public class Student {
+public class Student implements Serializable {
+    private Integer age;
+    private String name;
+
 }
