ecc041ba82ebd4676f969a301e88219ff6b7b628
zhaoss <chinazss@126.com>
2022-05-20 20:07:39 +0800

    在Person类中增加了age属性

---
+++ TestModule01/src/com/msb/person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @auther: zhaoss
+ * @date: 2022\5\20 0020 - 05 - 20 - 20:06
+ * @description: com.msb
+ * @version: 1.0
+ */
+public class person {
+    private int age;
+}
